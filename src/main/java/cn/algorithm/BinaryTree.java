2734a9033147048018dc60a59178475c75f7698c
wuqinduo <1085969352@qq.com>
2020-07-31 18:46:13 +0800

    learn

---
+++ src/main/java/cn/algorithm/BinaryTree.java
@@ -0,0 +1,11 @@
+package cn.algorithm;
+
+/**
+    二叉树：
+    二叉搜索树：
+    平衡二叉树：
+    B树：
+    B+树：
+ */
+public class BinaryTree {
+}
---
+++ src/main/java/cn/jvmlearn/G1_Test.java
@@ -0,0 +1,20 @@
+package cn.jvmlearn;
+
+/**
+ * Created by dell on 2020/7/29.
+ */
+public class G1_Test {
+
+    public static void main(String[] args) {
+        int size  = 1024* 1024;
+
+        byte[] myAlloc1 = new byte[size];
+        byte[] myAlloc2 = new byte[size];
+        byte[] myAlloc3 = new byte[size];
+        byte[] myAlloc4 = new byte[size];
+
+        System.out.println("helllo");
+
+
+    }
+}
